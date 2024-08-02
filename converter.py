7d39c1e580a2db0d070e6f5341ccecdfe2867db0
Swarup Saha <swarupsro@proton.me>
2024-08-02 09:21:31 +0600

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,32 +1,25 @@
 import os
 import requests
 
-# Your VirusTotal API key
 api_key = 'input the key from virus total'
 
-# File names
 input_filename = 'hashes.txt'
 output_filename = 'sha256_hashes.txt'
 
-# Get the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Construct full file paths
 input_file = os.path.join(script_dir, input_filename)
 output_file = os.path.join(script_dir, output_filename)
 
-# Base URL for VirusTotal API
 base_url = 'https://www.virustotal.com/api/v3/files/'
 
 headers = {
     'x-apikey': api_key
 }
 
-# Read hashes from the input file
 with open(input_file, 'r') as file:
     hashes = file.read().splitlines()
 
-# Open the output file in write mode
 with open(output_file, 'w') as file:
     for hash_value in hashes:
         response = requests.get(base_url + hash_value, headers=headers)
@@ -37,11 +30,9 @@ with open(output_file, 'w') as file:
         else:
             result = f'Error: Unable to fetch SHA-256 for {hash_value}'
         
-        # Write the SHA-256 hash to the output file if no error
         if 'Error' not in result:
             file.write(result + '\n')
-        
-        # Print the result to the console
+
         print(result)
 
 print(f'SHA-256 hashes written to {output_filename}')
